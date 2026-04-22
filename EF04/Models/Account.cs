a6d1ec226279fa6ff1fe973931434ce388085a3f
Zeyad Hussein <zeyad02_hussein@hotmail.com>
2026-04-23 01:22:33 +0200

    qouestion 1

---
+++ EF04/Models/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF04.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string AccountNumber { get; set; }
+        public string AccountType { get; set; }
+        public DateTime OpeningDate { get; set; }
+        public decimal CurrentBalance { get; set; }
+
+        public int BranchId { get; set; }
+        public Branch Branch { get; set; }
+
+        public ICollection<CustomerAccount> CustomerAccounts { get; set; }
+        public ICollection<Transaction> Transactions { get; set; }
+    }
+}
--- EF04/Program.cs
+++ EF04/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace EF04
+﻿using EF04.Data;
+using EF04.Models;
+
+namespace EF04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using var context = new AppDbContext();
+
+            #region Question 1: Add New Customer
+            var customer = new Customer
+            {
+                FullName = "Ahmed Ali",
+                NationalId = "123456789",
+                DateOfBirth = new DateTime(1995, 5, 1),
+                Email = "ahmed@test.com",
+                PhoneNumber = "0100000000",
+                Address = "Cairo",
+                CustomerType = "Individual"
+            };
+
+            context.Customers.Add(customer);
+            context.SaveChanges();
+            #endregion
         }
     }
 }
